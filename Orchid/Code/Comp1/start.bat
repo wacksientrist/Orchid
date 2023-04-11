50b744af60e5473fc897e95f6fcc7c1cfdf508dc
wacksientrist <82042695+wacksientrist@users.noreply.github.com>
2023-04-11 09:00:56 -0400

    Add files via upload

---
+++ Orchid/Code/Comp1/start.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+
+cd W:/Code/Orchid/Code/
+W:/Code/python.exe W:/Code/Orchid/Code/Comp1/Instance.py
---
+++ Orchid/Code/Public/Lib1.py
@@ -0,0 +1,26 @@
+import os
+import time
+
+class Instance():
+        def __init__(self, UUID):
+              self.UUID = UUID
+        def Process(self, A, B, Type):
+                A_fil = open("Comp"+str(self.UUID)+"/Instrc_r.txt", "w")
+                A_fil.write(str(A)+"!!!!!!!"+str(B)+"!!!!!!!"+Type)
+                A_fil.close()
+        def Read(self):
+                time.sleep(0.1)
+                A_fil = open("Comp"+str(self.UUID)+"/Instrc_s.txt", "r")
+                while(True):
+                        A_str = A_fil.read()
+                        if A_str == "":
+                                pass
+                        else:
+                                if A_str == "T":
+                                        A_fil.close()
+                                        return True
+                                if A_str == "F":
+                                        A_fil.close()
+                                        return False
+                                A_fil.close()
+                                return A_str
