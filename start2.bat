2c6af14a7832a364155eb65159b5d257d2368b3b
wacksientrist <82042695+wacksientrist@users.noreply.github.com>
2023-05-03 18:09:14 -0400

    Delete start2.bat

--- start2.bat
+++
@@ -1,2 +0,0 @@
-C:/Users/Administrator/AppData/Local/Programs/Python/Python311/python.exe C:/Users/Administrator/Desktop/Code/Orchestrator/Computer1/Code/Dig1_1.pyc
-C:/Users/Administrator/AppData/Local/Programs/Python/Python311/python.exe C:/Users/Administrator/Desktop/Code/Orchestrator/Computer1/Code/Dig2_1.pyc
